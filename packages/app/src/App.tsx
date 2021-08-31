36ad2b0f8ade859732eaa0111094a5063c46464b
mauro sala <mauro.sala@gmail.com>
2021-08-31 11:35:25 +0200

    feat(app): providers

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -6,11 +6,11 @@ import {
   CatalogIndexPage,
   catalogPlugin,
 } from '@backstage/plugin-catalog';
-import {CatalogImportPage, catalogImportPlugin} from '@backstage/plugin-catalog-import';
-import { 
-  ScaffolderPage, 
-  scaffolderPlugin 
-} from '@backstage/plugin-scaffolder';
+import {
+  CatalogImportPage,
+  catalogImportPlugin,
+} from '@backstage/plugin-catalog-import';
+import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { SearchPage } from '@backstage/plugin-search';
 import { TechRadarPage } from '@backstage/plugin-tech-radar';
 import { TechdocsPage } from '@backstage/plugin-techdocs';
@@ -19,8 +19,75 @@ import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInProviderConfig,
+} from '@backstage/core-components';
 import { createApp, FlatRoutes } from '@backstage/core-app-api';
+
+import {
+  githubAuthApiRef,
+  samlAuthApiRef,
+  gitlabAuthApiRef,
+} from '@backstage/core-plugin-api';
+
+const githubProvider: SignInProviderConfig = {
+  id: 'github-auth-provider',
+  title: 'GitHub',
+  message: 'Sign in using GitHub',
+  apiRef: githubAuthApiRef,
+};
+
+const gitlabProvider: SignInProviderConfig = {
+  id: 'gitlab-auth-provider',
+  title: 'GitLab',
+  message: 'Sign in using GitLab',
+  apiRef: gitlabAuthApiRef,
+};
+
+// const microsoftProvider: SignInProviderConfig = {
+//   id: 'microsoft-auth-provider',
+//   title: 'Microsoft',
+//   message: 'Sign in using Microsoft',
+//   apiRef: microsoftAuthApiRef,
+// };
+
+// const googleProvider: SignInProviderConfig = {
+//   id: 'google-auth-provider',
+//   title: 'Google',
+//   message: 'Sign in using Google',
+//   apiRef: googleAuthApiRef,
+// };
+
+const samlProvider: SignInProviderConfig = {
+  id: 'saml-auth-provider',
+  title: 'Saml',
+  message: 'Sign in using Saml',
+  apiRef: samlAuthApiRef,
+};
+
+/* AUTH */
+const enabledProviders: any = [];
+
+if (process.env.AUTH_GUEST === 'true') {
+  enabledProviders.push('guest');
+}
+if (process.env.AUTH_GITHUB_CLIENT_ID !== '') {
+  enabledProviders.push(githubProvider);
+}
+if (process.env.AUTH_GITLAB_CLIENT_ID !== '') {
+  enabledProviders.push(gitlabProvider);
+}
+// if (process.env.AUTH_GOOGLE_CLIENT_ID !== '') {
+//   enabledProviders.push(googleProvider);
+// }
+// if (process.env.AUTH_MICROSOFT_CLIENT_ID !== '') {
+//   enabledProviders.push(microsoftProvider);
+// }
+// if (process.env.AUTH_SAML_ENTRY_POINT !== '') {
+// enabledProviders.push(samlProvider);
+// }
 
 const app = createApp({
   apis,
